16ea72f5866e995326e13864b51297a96e9dcde3
nihar-hegde <niharhegde163@gmail.com>
2024-08-30 22:49:53 +0530

    minor design changes in the success page

--- src/components/Onboarding/Pages/SuccessPage.tsx
+++ src/components/Onboarding/Pages/SuccessPage.tsx
@@ -4,8 +4,13 @@ import React from "react";
 
 export const SuccessPage = () => {
   return (
-    <div>
-      <div>Success page go to dashboard</div>
+    <div className="flex flex-col items-center justify-center gap-8 bg-neutral-900 p-10 rounded-xl ">
+      <h1 className="text-2xl font-bold">
+        Your account creation was a success.
+      </h1>
+      <h2 className="text-xl font-semibold">
+        Go to dashboard to view more info about your account.
+      </h2>
       <Link href={"/dashboard"} className={buttonVariants()}>
         Dashboard
       </Link>
