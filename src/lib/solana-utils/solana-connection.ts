ac671fe0547966de2ee7fbfe67fae6908c1d6493
nihar-hegde <niharhegde163@gmail.com>
2024-09-02 20:41:53 +0530

    minor change

--- src/lib/solana-utils/solana-connection.ts
+++ src/lib/solana-utils/solana-connection.ts
@@ -2,11 +2,9 @@ import { Connection } from "@solana/web3.js";
 
 let connection: Connection | null = null;
 
-const key = "tqoA0HsqL3luasjokw97vxQxbgVyU_D6";
-
 export const getSolanaConnection = (): Connection => {
   if (!connection) {
-    const rpcEndpoint = `https://solana-devnet.g.alchemy.com/v2/${key}`;
+    const rpcEndpoint = process.env.NEXT_PUBLIC_SOLANA_RPC_ENDPOINT!;
     connection = new Connection(rpcEndpoint);
   }
 
